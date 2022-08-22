221ab985a2264ba199da2f2a6e99745144e4ba22
Lucas Santos <lucas.parkl@outlook.com>
2022-08-22 20:11:58 -0300

    atualização ex2.py

--- ex 2.py
+++ ex 2.py
@@ -3,20 +3,14 @@
 from operator import le
 
 
-with open ("teste2.c", "r") as file:
-    numeros = []
-    alfabeto = ["asm" , "auto" , "break" , "case" , "char" , "const" , "continue" , "default" , "do" , "double" , "else" , 
+with open ("teste2.c", "r") as file, open("teste2-maiusculo.c", "w+") as writefile:
+    alfabeto = ["main" , "auto" , "break" , "case" , "char" , "const" , "continue" , "default" , "do" , "double" , "else" , 
     "enum" ,"extern" ,"float" ,"for" ,"goto" ,"if" ,"int" ,"long" ,"register" ,"return" ,"short" ,"signed" ,"sizeof" ,"static" , "struct", "switch", "typedef" , "union" , "unsigned" , "void" , "volatile" , "while"]
-
-    count = [0 for i in range(len(alfabeto))] 
-    
-    
-
+ 
     for linhas in file:
-        print("linha: "+ linhas)
         for i in range(len(alfabeto)):
-            count[i] = count[i] + linhas.count(alfabeto[i])
+            linhas = linhas.replace(alfabeto[i], alfabeto[i].upper())
+        writefile.write(linhas)
 
-for i in range(len(alfabeto)):
-    print( alfabeto[i] + " : " + str(count[i]))
+
 
