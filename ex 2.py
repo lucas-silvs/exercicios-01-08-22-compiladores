3eaf91234341b7ff9c793162a50d84653f7d17dd
Lucas Santos <lucas.parkl@outlook.com>
2022-08-28 12:20:43 -0300

    add campos obrigatórios da ac e removendo comentários

--- ex 2.py
+++ ex 2.py
@@ -8,11 +8,6 @@ import re
 with open ("Lucas da Silva Santos - ex03.c", "r") as file, open("ex03.out", "w+") as writefile:
     alfabeto = ["main" , "auto" , "break" , "case" , "char" , "const" , "continue" , "default" , "do" , "double" , "else" , 
     "enum" ,"extern" ,"float" ,"for" ,"goto" ,"if" ,"int" ,"long" ,"register" ,"return" ,"short" ,"signed" ,"sizeof" ,"static" , "struct", "switch", "typedef" , "union" , "unsigned" , "void" , "volatile" , "while"]
-    
-    # for linhas in file:
-    #     for i in range(len(alfabeto)):
-    #         linhas = linhas.replace(alfabeto[i], alfabeto[i].upper())
-    #     writefile.write(linhas)
 
     for linhas in file:
         for i in range(len(alfabeto)):
--- ex1.py
+++ ex1.py
@@ -1,8 +1,6 @@
 import os
 
-
 with open ("Lucas da Silva Santos - ex03.c", "r") as file:
-    numeros = []
     alfabeto = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" ,"m" ,"n" ,"o" ,"p" ,"q" ,"r" ,"s" ,"t" ,"u" ,"v" ,"w" ,"x" ,"y" , "z", "1", "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9" , "0", " "]
 
     count = [0 for i in range(len(alfabeto))] 
